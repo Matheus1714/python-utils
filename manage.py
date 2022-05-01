d5b09dbd803ada4ee2fdc00a7bb31bb00639cf0c
matheus1714 <matheussilvamartins1714@gmail.com>
2022-05-01 20:35:55 -0300

    simplify manage file

--- manage.py
+++ manage.py
@@ -1,12 +1,9 @@
 import sys
 from pdf_utils.join_pdfs import join_pdfs
+from pdf_utils.verify_pdf_file import verify_pdf_file
 
 def main(*args, **kwargs):
-    cmds = list(args[0])
-    if cmds[1] == 'run':
-        if len(cmds) >= 3:
-            if cmds[2] == 'join_pdfs':
-                join_pdfs()
+    pass
 
 if __name__ == '__main__':
     main(sys.argv)
