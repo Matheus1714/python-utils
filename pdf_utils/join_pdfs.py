ec6f8f69ece41a99c292a736144372b2ca2274f5
matheus1714 <matheussilvamartins1714@gmail.com>
2022-05-01 16:28:40 -0300

    adapte join pdf to clean code

--- pdf_utils/join_pdfs.py
+++ pdf_utils/join_pdfs.py
@@ -1,15 +1,22 @@
 from PyPDF2 import PdfFileMerger
-import os
 
-def join_pdfs():
+from .constants import input_path, result_path
+from file_utils.get_files_with_extension import get_files_with_extension
+
+def join_pdfs(*args, **kwargs)->None:
     
-    input_path = os.getcwd() + '\\pdf_utils\\inputs\\'
-    result_path = os.getcwd() + '\\outputs\\'
-    input_files = os.listdir(input_path)
+    result_file_name = "result"
+    if 'result_file_name' in kwargs:
+        result_file_name:str
+        result_file_name = kwargs['result_file_name']
+
+    input_files = get_files_with_extension(input_path)
 
     merger = PdfFileMerger()
     for inputs_file in input_files:
         merger.append(input_path + inputs_file)
 
-    merger.write(result_path + "result.pdf")
+    output_path = result_path + result_file_name + '.pdf'
+    merger.write(output_path)
+
     merger.close()
